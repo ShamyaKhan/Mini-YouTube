5d11096604bc546dbe11d90635a80c8ddf880603
Shamya Khan <md.shamiulhaque127@gmail.com>
2026-03-11 18:08:47 +0600

    search api fixed

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -3,6 +3,6 @@ export const YOUTUBE_VIDEOS_API =
   import.meta.env.VITE_YOUTUBE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const LIVE_CHAT_COUNT = 20;
